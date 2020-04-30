8bc7332792ac5d091c734f76332ee00cef7fd070
Bruno Martínez <bmartinezg@minsait.com>
2020-04-30 19:50:52 +0200

    grandisimos avances en la creacion de producto

--- src/components/admin/products/ItemForm.vue
+++ src/components/admin/products/ItemForm.vue
@@ -19,17 +19,19 @@
         b-form-input( v-model="form.purchasePrice" type="number" required)
 
       b-form-group( label="Precio de venta")
-        b-form-input(v-model="form.salePrice" type="number" required)
-
-      b-form-group( label="Porcentaje")
-        b-form-input( v-model="form.percentage" type="number" required)
+        b-form-input(v-model="form.salePrice" type="number" required) {{ calculateDiscount }}
 
       b-form-group( label="En promocion")
         b-form-checkbox(
           v-model="form.promotionOn" 
           name="promotionOn"
-          value="true"
-          unchecked-value="false")
+          )
+          
+      b-form-group( label="Porcentaje")
+        b-form-input(:disabled="!form.promotionOn" v-model="form.percentage" type="number" required)
+
+      b-form-group( v-if="form.promotionOn" label="Precio con porcentaje aplicado")
+        b-form-input(disabled v-model="form.priceWithDiscount" type="number")
 
       b-form-group( label="Numero de referencia")
         b-form-input(required  v-model="form.referenceNumberCommon")
@@ -43,12 +45,13 @@
 
       b-form-group( label="Accesorio")
         b-form-checkbox(
-          name="promotionOn"
-          value="true"
-          v-model="form.accesory"
-          unchecked-value="false")
+          name="Accesorio"
+          v-model="form.accesory")
   
-      .form__stock
+      b-form-group( v-if="form.accesory" label="Total accesorios")
+        b-form-input(v-model="totalStock" type="number")
+  
+      .form__stock(v-if="!form.accesory")
         b-form-group( label="Stock")
           hr
           b-row(v-for="(item, index) in form.stock" :key="index" )
@@ -82,10 +85,15 @@
                   i(class="fas fa-plus")
                 b-button(variant="link" @click="removeQuantity(index)")
                   i(class="fas fa-minus")
+      b-form-group( label="Imagen")
+        b-form-file( ref="file" v-model="form.img" type="file" accept="image/jpeg, image/png")
+      br
+      br
       b-button(variant="outline-primary" type="submit") Crear
 
 </template>
 <script>
+import axios from 'axios'
 import {mapActions, mapGetters} from 'vuex'
 export default {
   name: 'ItemForm',
@@ -102,6 +110,7 @@ export default {
         promotionOn: false,
         referenceNumberCommon: '',
         description: '',
+        totalStock: 0,
         stock: [
           {ref: '', color: '', quantity: [ 
             {size: '', units: 0}
@@ -109,16 +118,53 @@ export default {
         ],
         accesory: false,
         category: '',
-        img: ''
+        img: null,
+        priceWithDiscount: ''
       }
     }
   },
   computed: {
-    ...mapGetters(['categories'])
+    ...mapGetters(['categories']),
+    calculateDiscount () {
+      if (this.form.promotionOn) {
+        let final = this.form.salePrice * (this.form.percentage / 100)
+        this.form.priceWithDiscount = this.form.salePrice - final
+        return this.form.priceWithDiscount
+      }
+    }
   },
   methods: {
     ...mapActions(['getCategories']),
-    onSubmit () {
+    // selectImg () {
+    //   console.log(this.$refs.file.files);
+    //   this.form.img = this.$refs.file.files[0]
+    // },
+    async onSubmit () {
+      const formData = new FormData()
+      formData.append('provider', this.form.provider)
+      formData.append('buyDate', this.form.buyDate)
+      formData.append('season', this.form.season)
+      formData.append('name', this.form.name)
+      formData.append('purchasePrice', this.form.purchasePrice)
+      formData.append('salePrice', this.form.priceWithDiscount || this.form.salePrice)
+      formData.append('percentage', this.form.percentage)
+      formData.append('promotionOn', this.form.promotionOn)
+      formData.append('referenceNumberCommon', this.form.referenceNumberCommon)
+      formData.append('description', this.form.description)
+      formData.append('stock', this.form.stock)
+      formData.append('accesory', this.form.accesory)
+      formData.append('category', this.form.category)
+      formData.append('img', this.form.img)
+
+      // Calcular el total de todos los productos si no es accesorio
+      if (!this.form.accesory) {
+        this.form.stock.forEach((item) => {
+          item.quantity.forEach((element) => {
+            this.form.totalStock += Number(element.units)
+          })
+        })
+      }
+      //await axios.post('http://localhost:3000/' , formData)
       console.log(this.form);
     },
     addQuantity (i) {
